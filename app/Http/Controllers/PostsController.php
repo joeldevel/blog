681fc7c23b9ec0be4e85960828af1041a5d7bb01
AlMoqanna <micorreodelauba@gmail.com>
2020-04-24 07:15:30 -0300

    posts page and post added

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+// use DB;
 
 class PostsController extends Controller
 {
@@ -13,7 +15,11 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        // Fetch the posts using eloquent
+        $posts = Post::orderBy('title', 'desc')->get();
+        // The same can be done without eloquent, just bring DB
+        // $posts = DB::select("SELECT * FROM posts");
+        return view('posts.index')->with('posts',$posts);
     }
 
     /**
@@ -45,7 +51,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.show')->with('post',$post);
     }
 
     /**
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // If you will you can change  the table fields names like this,
+    // but  here is not needed
+    // protected $table = 'posts';
+    // PK
+    // public $primaryKey = 'id';
+    // Timestamps
+    public $timestamps = true;
 }
