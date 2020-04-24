c96db823bf8cc9a8ad77f99e49c3ae0df9990aac
AlMoqanna <micorreodelauba@gmail.com>
2020-04-24 06:30:29 -0300

    style with bootstrap , added navbar

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Home</h1>
-  <p>This is a blog for practicing laravel</p>
+  <div class="jumbotron text-center">
+    <h1>Home</h1>
+    <p>This is a blog for practicing laravel</p>
+    <p>
+      <a href="/login" class="btn btn-primary btn-lg">login</a>
+      <a href="/register" class="btn btn-primary btn-lg">register</a>
+    </p>
+  </div>
 @endsection
