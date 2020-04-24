dc538a575add63725a48064a7353b1843f5c919e
AlMoqanna <micorreodelauba@gmail.com>
2020-04-24 11:40:35 -0300

    Added cover_image to posts table

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <h1>Creat a post</h1>
 
-<form method="post" action="{{ route('posts.store') }}">
+<form method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
    <div class="form-group">
      @csrf
      <label for="title">Title</label>
@@ -13,6 +13,9 @@
      <label for="summary-ckeditor">Body</label>
      <textarea class="form-control" name="summary-ckeditor"  cols="30" rows="10"
       placeholder="Body Text"></textarea>
+   </div>
+   <div class="form-group">
+     <input type="file" name="cover_image" value="">
    </div>
    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
